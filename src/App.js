7b6825a3e73a1ae3fcd47bd0b221288e1dc6c70c
zuihuanxiaocao <dragonleng@163.com>
2019-12-28 20:40:20 +0800

    用forEach方式

--- src/App.js
+++ src/App.js
@@ -4,20 +4,21 @@ import React, { Component } from 'react'
 import './App.css'
 export default class App extends Component {
   render() {
-    let titles = ["Pullrequest","lsssues","Maketplace","Explore"]
-    let forList=[]
-    
+    let titles = ["Pullrequest", "lsssues", "Maketplace", "Explore"]
+    let forList = []
+    titles.forEach((v, i) => forList.push(<li key={i}>{v}</li>))
     return (
       <div>
-      <ul>
-        {/* <li>Pullrequest</li>
-        <li>lsssues</li>
-        <li>Maketplace</li>
-        <li>Explore</li> */
-          titles.map((v,i)=> <li key={i}>{v}</li>)
-          //foreach返回值是空，要push
-        // titles.forEach((v,i)=> <li key={i}>{v}</li>)
-        }
+        <ul>
+          {/* <li>Pullrequest</li>
+              <li>lsssues</li>
+              <li>Maketplace</li>
+              <li>Explore</li> */
+            // titles.map((v, i) => <li key={i}>{v}</li>)
+            //foreach返回值是空，要push到一个空数组
+            // titles.forEach((v,i)=> <li key={i}>{v}</li>)
+            forList
+          }
         </ul>
       </div>
     )
